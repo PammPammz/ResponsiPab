b647b03807acd7106fc9a4cad8ae4e5fb8f1a488
Belajar Git <rifalmuhammad517@student.uns.ac.id>
2025-05-26 20:27:16 +0700

    tambahan ui loading screen di awal bukak

--- app/src/main/java/com/example/responsipab/MainActivity.kt
+++ app/src/main/java/com/example/responsipab/MainActivity.kt
@@ -34,7 +34,7 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.responsipab.ui.theme.RentalKameraTheme
+import com.example.rentalkamera.ui.theme.RentalKameraTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/responsipab/ui/theme/Theme.kt
+++ app/src/main/java/com/example/responsipab/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.example.responsipab.ui.theme
+package com.example.rentalkamera.ui.theme
 
 import android.app.Activity
 import android.os.Build
